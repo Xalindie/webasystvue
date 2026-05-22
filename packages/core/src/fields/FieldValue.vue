f1d1ac11dd0a93b915ab2ced43bd3bebbf0eedd7
Ilya Serdyukov <ilyaserdyukovmmm@gmail.com>
2026-05-22 19:11:34 +1100

    feat: fields component (#101)

---
+++ packages/core/src/fields/FieldValue.vue
@@ -0,0 +1,30 @@
+<template>
+    <Primitive
+        :class="clsx(fieldValueVariants({ middle, submit }), props.class)"
+        v-bind="delegatedProps"
+    >
+        <slot/>
+    </Primitive>
+</template>
+
+<script lang="ts">
+import type { PrimitiveProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+
+export interface FieldValueProps extends PrimitiveProps {
+    class?: HTMLAttributes['class']
+    middle?: boolean
+    submit?: boolean
+}
+</script>
+
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { clsx } from 'clsx'
+import { Primitive } from 'reka-ui'
+import { fieldValueVariants } from '.'
+
+const props = defineProps<FieldValueProps>()
+
+const delegatedProps = reactiveOmit(props, 'middle', 'submit')
+</script>
---
+++ packages/core/src/fields/Fields.vue
@@ -0,0 +1,29 @@
+<template>
+    <Primitive
+        :class="clsx(fieldsVariants({ vertical }), props.class)"
+        v-bind="delegatedProps"
+    >
+        <slot/>
+    </Primitive>
+</template>
+
+<script lang="ts">
+import type { PrimitiveProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+
+export interface FieldsProps extends PrimitiveProps {
+    class?: HTMLAttributes['class']
+    vertical?: boolean
+}
+</script>
+
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { clsx } from 'clsx'
+import { Primitive } from 'reka-ui'
+import { fieldsVariants } from '.'
+
+const props = defineProps<FieldsProps>()
+
+const delegatedProps = reactiveOmit(props, 'vertical')
+</script>
